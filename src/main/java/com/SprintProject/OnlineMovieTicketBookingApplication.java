e4e220371ccc1dd370c75837c5637d72ab2a6ed5
Ashmitha <ashmithashree06@gmail.com>
2021-09-21 10:18:10 +0530

    first commit

---
+++ src/main/java/com/SprintProject/OnlineMovieTicketBookingApplication.java
@@ -0,0 +1,13 @@
+package com.SprintProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMovieTicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMovieTicketBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SprintProject/OnlineMovieTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SprintProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMovieTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
