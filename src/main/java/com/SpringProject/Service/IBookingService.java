03d6004c6048cf0f8e86b479cd46f73175f24666
Ashmitha <ashmithashree06@gmail.com>
2021-09-22 15:59:02 +0530

    local commit

--- src/main/java/com/SpringProject/Service/IBookingService.java
+++
@@ -1,5 +0,0 @@
-package com.SpringProject.Service;
-
-public interface IBookingService {
-	public abstract TicketBooking addBooking(TicketBooking booking);
-}
--- src/main/java/com/SpringProject/Service/ISeatService.java
+++
@@ -1,7 +0,0 @@
-package com.SpringProject.Service;
-
-public interface ISeatService {
-	public abstract Seat bookSeat(Seat seat);
-	public abstract  Seat cancleSeatBooking(Seat seat);
-	public Seat blockSeat(Seat seat);
-}
