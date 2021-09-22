19528e8783e724dc2ed8d2c28c638ef6a16baebb
Ashmitha <ashmithashree06@gmail.com>
2021-09-22 16:45:06 +0530

    Entities changed

---
+++ src/main/java/com/SprintProject/entities/User.java
@@ -0,0 +1,57 @@
+package com.SprintProject.entities;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity(name="onlineuser")
+@Table(name="onlineuser")
+public class User {
+	private static final long serialVersionUID =1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int userId;
+	@OneToOne(cascade = CascadeType.ALL,fetch=FetchType.LAZY)
+	@JoinColumn(name="customer_id")
+	private Customer customerId;
+	private String password;
+	private String role;
+	
+	public int getUserId() {
+		return userId;
+	}
+	
+	public Customer getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(Customer customerId) {
+		this.customerId = customerId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+}
